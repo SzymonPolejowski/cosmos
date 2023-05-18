36d282ce46dfae6c99872f75a6ebcf7b06f8dbb4
spolejox <szymonx.polejowski@intel.com>
2023-05-18 12:57:57 +0200

    init

---
+++ src/test/java/com/cosmos/solaris/SolarisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cosmos.solaris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolarisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
