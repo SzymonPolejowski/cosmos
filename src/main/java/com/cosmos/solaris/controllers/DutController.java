c3e28b5652854b11be7ace0270be115402b75e20
spolejox <szymonx.polejowski@intel.com>
2023-06-09 15:53:14 +0200

    Added option to retrieve one DutEntity

--- src/main/java/com/cosmos/solaris/controllers/DutController.java
+++ src/main/java/com/cosmos/solaris/controllers/DutController.java
@@ -2,8 +2,11 @@ package com.cosmos.solaris.controllers;
 
 import com.cosmos.solaris.database.DutJPARepository;
 import com.cosmos.solaris.database.entities.DutEntity;
+import com.cosmos.solaris.exceptions.FieldNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -20,6 +23,16 @@ public class DutController {
 	@GetMapping("/duts")
 	public List<DutEntity> retrieveDuts(){
 		return repository.findAll();
+	}
+
+	@GetMapping("/duts/{id}")
+	@ResponseBody
+	public DutEntity retrieveOne(@PathVariable int id){
+		DutEntity returning = repository.findById(id).orElse(null);
+		if(returning == null){
+			throw new FieldNotFoundException("Field with requested id: " + id + " was not found.");
+		}
+		return returning;
 	}
 
 }
